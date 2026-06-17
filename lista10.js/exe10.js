d127811f51477fcad48e237640593459a688ab4f
evelyn870 <evelynbarbosa749@gmail.com>
2026-06-17 20:43:40 -0300

    Atualizar o exe10.js

--- lista10.js/exe10.js
+++ lista10.js/exe10.js
@@ -1,14 +1,23 @@
 const jogadores = [
-  { nome: "Carlos", pontos: 150 },
-  { nome: "Ana", pontos: 320 },
-  { nome: "Bruno", pontos: 210 },
-  { nome: "Marina", pontos: 500 }
+    { nome: "Evelyn", pontos: 120 },
+    { nome: "Giovanna", pontos: 80 },
+    { nome: "Gustavo", pontos: 150 },
+    { nome: "Lucas", pontos: 100 }
 ];
 
-function rankingPontuacao(lista) {
-  return lista.sort((a, b) => b.pontos - a.pontos);
+function ranking(lista) {
+for (let i = 0; i < lista.length; i++) {
+for (let j = i + 1; j < lista.length; j++) {
+
+  if (lista[j].pontos > lista[i].pontos) {
+
+   let aux = lista[i];
+   lista[i] = lista[j];
+   lista[j] = aux; }
+}
 }
 
-const ranking = rankingPontuacao(jogadores);
+    return lista;
+}
 
-console.log(ranking);
+console.log(ranking(jogadores));
