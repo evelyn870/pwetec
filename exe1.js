cafc2dd2191562df9b95d07f92455ad0db4a3799
evelyn870 <evelynbarbosa749@gmail.com>
2026-06-17 18:36:35 -0300

    Add files via upload

---
+++ exe1.js
@@ -0,0 +1,10 @@
+let RepresAluno = {
+    nome: "Kaike", 
+    idade:  "19",
+    curso: "DS",
+};
+
+function frase (RepresAluno){
+    console.log("Olá, meu nome é" + nome + ", tenho " + idade + "anos" + "e curso" + curso);
+}
+
