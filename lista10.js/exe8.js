e155641e7d8b63b8e6631fb2fee29352e398de29
evelyn870 <evelynbarbosa749@gmail.com>
2026-06-17 20:29:25 -0300

    Replace pessoas array with new data

--- lista10.js/exe8.js
+++ lista10.js/exe8.js
@@ -1,9 +1,9 @@
 let pessoas = [
-    { nome: "Ana", idade: 15 },
-    { nome: "Bruno", idade: 22 },
-    { nome: "Carlos", idade: 18 },
-    { nome: "Diana", idade: 25 },
-    { nome: "Eduardo", idade: 30 }
+    { nome: "Gustavo", idade: 15 },
+    { nome: "Evelyn", idade: 22 },
+    { nome: "Giovanna", idade: 18 },
+    { nome: "Dany", idade: 25 },
+    { nome: "Cris", idade: 30 }
 ];
 
 function filtrarMaiores18(lista) {
